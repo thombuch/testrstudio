f623ad4d14f85407dbb3dbe60f22b6466823af7e
Thom Buchmueller <thom@mykolab.ch>
2019-03-29 20:14:00 +0100

    2nd commit

--- Testing.R
+++ Testing.R
@@ -1,2 +1,3 @@
 print("This file was created within RStudio")
-print("And now it lives on GitHub")
+print("And now it lives on GitHub - and all its descendants")
+print("a third line in the second commit")
